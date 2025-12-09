1a77386310c387b02950a541e41c21359576db50
anhlehuyrn <leekarry2004@gmail.com>
2025-12-09 21:22:45 +0800

    Update smart blink project files

--- linkwweb.py
+++ linkwweb.py
@@ -4,7 +4,21 @@ print("Hello World!")
 python -m http.server 8000 
 http://localhost:8001/index.html#intro
 
+
+
+
+
+
+
+cd "C:\Users\leeka\Documents\0. ER pl\smart blink"
+
+# Stage all changes
 git add .
-git commit -m "Update code"
-git push
+
+# Commit with a message
+git commit -m "Update smart blink project files"
+
+# Push to GitHub
+git push origin main
+
 """
